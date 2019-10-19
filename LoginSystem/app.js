34304ef1bf9f68aeaee6adadb1cc5df30c537fb5
Chaichana <49665324+sun-23@users.noreply.github.com>
2019-10-19 22:53:11 +0700

    LoginSystemTutorial
    ok

---
+++ LoginSystem/app.js
@@ -0,0 +1,60 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+var bcrypt = require('bcryptjs');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+
+app.get('*',function(req,res,next){
+  res.locals.user = req.user || null;
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+
+module.exports = app;
---
+++ LoginSystem/routes/users.js
@@ -0,0 +1,100 @@
+// router
+var express = require('express');
+var router = express.Router();
+var User = require('../model/user');
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+const {
+  check,
+  validationResult
+} = require('express-validator');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register.ejs');
+});
+
+router.post('/register', [
+  check('email', 'กรุณาป้อนอีเมล').isEmail(),
+  check('name', 'กรุณาป้อนชื่อของท่าน').not().isEmpty(),
+  check('password', 'กรุณาป้อนรหัสผ่าน').not().isEmpty()
+], function(req, res, next) {
+  const result = validationResult(req);
+  var errors = result.errors;
+  //Validation Data
+  if (!result.isEmpty()) {
+    //Return error to views
+    res.render('register', {
+      errors: errors
+    });
+  } else {
+    //Insert Data
+    var name = req.body.name;
+    var password = req.body.password;
+    var email = req.body.email;
+    var newUser = new User({
+      name: name,
+      password: password,
+      email: email
+    });
+    User.createUser(newUser, function(err, user) {
+      if (err) throw err ;
+    });
+    res.location('/');
+    res.redirect('/');
+
+  }
+
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login.ejs');
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/users/login');
+});
+
+router.post('/login', passport.authenticate('local',{
+  failureRedirect: '/users/login',
+    failureFlash: true
+}),function(req, res, next) {
+  req.flash("success","ลงชื่อเข้าใช็เรียบร้อย");
+  res.redirect('/');
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  User.getUserByName(username, function(err, user) {
+    if (err) throw error;
+    if (!user) {
+        return done(null, false);
+    }else{
+      User.comparePassword(password, user.password, function(err, isMatch) {
+        if (err) return err;
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      });
+    }
+  });
+}));
+module.exports = router;
